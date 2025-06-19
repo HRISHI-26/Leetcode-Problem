601d01557acc343b4a103659c2a7b3d1e00c520d
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-06-19 15:06:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.52%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.c
@@ -0,0 +1,13 @@
+#include<string.h>
+int numJewelsInStones(char* jewels, char* stones) {
+    int count = 0;
+    int jewels_count = strlen(jewels);
+    int stones_count = strlen(stones);
+    for (int i = 0; i < jewels_count; i++) {
+        for (int j = 0; j < stones_count; j++) {
+            if (jewels[i] == stones[j])
+                count++;
+        }
+    }
+    return count;
+}
