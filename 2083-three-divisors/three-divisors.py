264e4ab07d7757c3e604e8601710a14bf76ae487
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-08-05 08:43:07 +0530

    Time: 3 ms (56.84%) | Memory: 17.8 MB (61.62%) - LeetSync

--- 2083-three-divisors/three-divisors.py
+++ 2083-three-divisors/three-divisors.py
@@ -1,6 +1,6 @@
 class Solution:
     def isThree(self, n: int) -> bool:
-        rem = [n % i for i in range(1, n + 1)]
-        if rem.count(0) == 3:
+        rem = [n % i for i in range(1, int(n/2) + 1)]
+        if rem.count(0) + 1 == 3:
             return True
         return False
