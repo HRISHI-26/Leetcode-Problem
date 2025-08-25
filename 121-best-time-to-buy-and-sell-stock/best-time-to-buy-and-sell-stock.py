7fd778e016de8847384449deb7989c10aba1c438
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-08-25 06:34:59 +0530

    Time: 99 ms (45.17%) | Memory: 26.8 MB (77.67%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        #Two-pointer approach
+        left, right = 0, 1
+        profit = 0
+
+        while right < len(prices):
+            #profitable
+            if prices[right] > prices[left]:
+                profit = max(profit, prices[right] - prices[left])
+            else: #non-profitable
+                left = right
+            right += 1
+        return profit
