4794c0864175369114dad2cffec9bc1a2b8917a7
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-08-27 08:46:19 +0530

    Time: 3 ms (12.47%) | Memory: 17.8 MB (61.59%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -4,7 +4,9 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         # Check if given list is None
         if not list1:
             return list2
@@ -15,16 +17,20 @@ class Solution:
         while currentNode.next:
             currentNode = currentNode.next
         currentNode.next = list2
-        
+
         # Add elements into array
         array = []
         currentNode = list1
         while currentNode:
             array.append(currentNode.val)
             currentNode = currentNode.next
-        
+
         # Sort array
-        array.sort()
+        n = len(array)
+        for i in range(n-1):
+            for j in range(i, n):
+                if array[i] > array[j]:
+                    array[i], array[j] = array[j], array[i]
         i = 0
 
         # Replace elements by sorted array
@@ -33,5 +39,5 @@ class Solution:
             currentNode.val = array[i]
             i += 1
             currentNode = currentNode.next
-        
+
         return list1
