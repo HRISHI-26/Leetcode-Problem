9523118ca09b4f60f07f637a5441e783317f162b
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-10-07 05:10:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (65.51%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.py
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -3,11 +3,11 @@ class Solution:
         count = 0
 
         if a > b:
-            for num in range(1, a+1):
+            for num in range(1, b+1):
                 if a % num == 0 and b % num == 0:
                     count += 1
         else:
-            for num in range(1, b+1):
+            for num in range(1, a+1):
                 if a % num == 0 and b % num == 0:
                     count += 1
         
