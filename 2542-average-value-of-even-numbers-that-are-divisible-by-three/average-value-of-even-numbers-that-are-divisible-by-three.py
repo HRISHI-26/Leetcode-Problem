ecd3530efb8bf3ef2d265a2ffcbd4bbe8532c4ce
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-10-07 04:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (92.67%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.py
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -1,13 +1,16 @@
 class Solution:
     def averageValue(self, nums: List[int]) -> int:
-        sum = 0
-        count = 0
-        for i in nums:
-            if i % 2 == 0 and i % 3 == 0:
-                sum += i
-                count += 1
-        if count != 0:
-            avg = int(sum / count)
-            return avg
-        else:
-            return 0
+        sum_of_elements = 0
+        count_of_elements = 0
+        average = 0
+
+        for number in nums:
+            if number % 2 == 0 and number % 3 == 0:
+                count_of_elements += 1
+                sum_of_elements += number
+
+        if count_of_elements != 0:
+            average = sum_of_elements / count_of_elements
+            return int(average)
+
+        return 0
