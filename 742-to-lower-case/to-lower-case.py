43c6cdc9c71c8cee886ab940a2ff15384748628f
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-10-10 20:32:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (99.59%) - LeetSync

--- 742-to-lower-case/to-lower-case.py
+++ 742-to-lower-case/to-lower-case.py
@@ -1,10 +1,3 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-        ls = ""
-        for i in s:
-            if ord(i) <= 90 and ord(i) >= 65: 
-                c = ord(i) 
-                c += 32
-                i = chr(c)
-            ls += i
-        return ls
+        return s.lower()
