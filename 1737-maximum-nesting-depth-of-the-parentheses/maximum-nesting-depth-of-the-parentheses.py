1041441af8b0d25c048f0ae870f40e8594fcae1f
HRISHINANDAN N <137533306+HRISHI-26@users.noreply.github.com>
2025-10-18 11:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.64%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,13 +1,12 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        counter = 0
+        count = 0
         maximum = 0
-        for symbol in s:
-            if symbol == "(" or symbol == ")":
-                if symbol == "(":
-                    counter += 1
-                    maximum = max(maximum, counter)
-                elif symbol == ")":
-                    counter -= 1
-        
+        for char in s:
+            if char == "(":
+                count += 1
+                if count > maximum:
+                    maximum = count
+            elif char ==")":
+                count -= 1
         return maximum
